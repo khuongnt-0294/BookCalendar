b660d88f6a827db78ecf7326326f8fba0dde7e82
KhuongNT <khuongnt1688@gmail.com>
2015-08-27 08:55:00 +0700

    Create static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title page_title
+    base_title = "BookCalendar"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
