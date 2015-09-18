0e8a413090b3a88e03c06f14aea05d8272d33e18
KhuongNT <khuongnt1688@gmail.com>
2015-09-18 14:29:06 +0700

    create event by click

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,18 +5,19 @@ class Schedule < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 150}
   validates :start_time, presence: true
   validates :finish_time, presence: true
+  validates :description, presence: true, length: {maximum: 450} 
   validates :room, presence: true
   validates :user, presence: true
 
   scope :today_schedule, -> {where("start_time LIKE ?", "%#{Time.now.to_date.to_s}%")}
 
-  def as_json(options = {})  
-   {  
-    :id => self.id,  
-    :title => self.title,  
-    :description => self.description || "",  
-    :start => starts_at.rfc822,  
-    :end => ends_at.rfc822,
-   }  
-  end 
+  # def as_json(options = {})  
+  #  {  
+  #   :id => self.id,  
+  #   :title => self.title,  
+  #   :description => self.description || "",  
+  #   :start => starts_at.rfc822,  
+  #   :end => ends_at.rfc822,
+  #  }  
+  # end 
 end
